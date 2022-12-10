17b2d0ccd222128a268f59b5fdb03add643b2367
Jtune89 <Joseph.tune1@gmail.com>
2022-12-10 15:26:20 +0000

    styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,28 +4,29 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-            <img src={logo} alt="logo" height="100" />
+            <img src={logo} alt="logo" height="90" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="justify-content-end flex-grow-1 pe-3">
             <Nav.Link> <i className="fas fa-home"></i> Home</Nav.Link>
-            <Nav.Link href="#link"> <i class="fa-solid fa-address-card"></i> About</Nav.Link>
+            <Nav.Link> <i className="fa-solid fa-address-card"></i> About</Nav.Link>
             <NavDropdown title="User">
-              <NavDropdown.Item href="#action/3.1"> 
+              <NavDropdown.Item> 
               <i className="fas fa-sign-in-alt"></i> Sign In
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item>
               <i className="fas fa-user-plus"></i> Register
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3"> 
+              <NavDropdown.Item> 
               <i className="fa-solid fa-user"></i> My Page
               </NavDropdown.Item>
             </NavDropdown>
