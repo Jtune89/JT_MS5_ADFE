89307f2d1c5fe3522d0c49e7488f39c0862017ca
Jtune89 <Joseph.tune1@gmail.com>
2022-12-10 13:37:55 +0000

    navigation set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1><Button variant="primary">Primary</Button></h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>Blah Blah</div>
+  )
+}
+
+export default NavBar
