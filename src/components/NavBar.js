7fd971c81e85d3440bbb1e77539d2ff10408a982
Jtune89 <Joseph.tune1@gmail.com>
2022-12-10 14:13:00 +0000

    nav bar updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,26 +3,28 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+            <img src={logo} alt="logo" height="100" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link> <i className="fas fa-home"></i> Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown title="User" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Sign In</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Register
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#action/3.3">
+                My Page
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
