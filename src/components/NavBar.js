d010952d93209ee0c79b776f1f1ac9b1ffc4e4d6
Jtune89 <Joseph.tune1@gmail.com>
2022-12-10 16:41:08 +0000

    nav styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css';
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
@@ -15,7 +12,7 @@ const NavBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav className="ms-auto">
             <Nav.Link> <i className="fas fa-home"></i> Home</Nav.Link>
             <Nav.Link> <i className="fa-solid fa-address-card"></i> About</Nav.Link>
             <NavDropdown title="User">
@@ -26,7 +23,7 @@ const NavBar = () => {
               <i className="fas fa-user-plus"></i> Register
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item> 
+              <NavDropdown.Item>
               <i className="fa-solid fa-user"></i> My Page
               </NavDropdown.Item>
             </NavDropdown>
