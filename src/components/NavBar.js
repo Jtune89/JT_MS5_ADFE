319c227ea186902b52ff1f16ed8efb474ec5556d
Jtune89 <Joseph.tune1@gmail.com>
2022-12-11 10:12:09 +0000

    styling elements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,28 +2,30 @@ import React from 'react'
 import styles from '../styles/NavBar.module.css';
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from '../assets/logo.png';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+        <NavLink to='/'>
         <Navbar.Brand>
             <img src={logo} alt="logo" height="90" />
-        </Navbar.Brand>
+        </Navbar.Brand></NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link> <i className="fas fa-home"></i> Home</Nav.Link>
-            <Nav.Link> <i className="fa-solid fa-address-card"></i> About</Nav.Link>
+            <NavLink to ='/'> <i className="fas fa-home"></i> Home</NavLink>
+            <NavLink to ='/about'> <i className="fa-solid fa-address-card"></i> About</NavLink>
             <NavDropdown title="User">
-              <NavDropdown.Item> 
+              <NavDropdown.Item to='/signin'> 
               <i className="fas fa-sign-in-alt"></i> Sign In
               </NavDropdown.Item>
-              <NavDropdown.Item>
+              <NavDropdown.Item to='/register'>
               <i className="fas fa-user-plus"></i> Register
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item>
+              <NavDropdown.Item to='/mypage'>
               <i className="fa-solid fa-user"></i> My Page
               </NavDropdown.Item>
             </NavDropdown>
