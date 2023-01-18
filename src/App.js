770a47d5592520b3e6470c895db2534218087c59
Jtune89 <Joseph.tune1@gmail.com>
2023-01-18 10:24:39 +0000

    build for activity page so commented on posts are displayed to user

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
-import Notifications from "./pages/notifications/Notifications";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -36,13 +35,17 @@ function App() {
               {<PostsPage message="No results found. Adjust the search word or like a post" 
               filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} />} />
 
-              <Route path="/commented" element= 
+            <Route path="/commented" element= 
               {<PostsPage message="No results found. Adjust the search word or like a post" 
               filter={`comment__owner__profile=${profile_id}&ordering=-comment__created_at&`} />} />
 
+            <Route path="/activity" element= 
+              {<PostsPage message="No comments on your posts" 
+              filter={`owner__profile=${profile_id}&comment__owner__profile=${profile_id}&
+              ordering=comment__created_at`} />} />
+
             <Route path="/posts/:id/edit" element={<PostEditForm />} />
             <Route path="/signin" element= {<SignInForm />} />
-            <Route path="/notifications" element= {<Notifications/>} />
             <Route path="/register" element= {<SignUpForm />} />
             <Route path="/posts/create" element={<PostCreateForm />} />
             <Route path="/posts/:id" element={<PostPage />} />
